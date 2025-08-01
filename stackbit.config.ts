8497fe29cabf8572527c23e3684f26547c1b610e
YJush <unsystematic30@gmail.com>
2025-08-01 22:19:47 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,58 +1,119 @@
 import { defineStackbitConfig, DocumentStringLikeFieldNonLocalized, SiteMapEntry } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from 'sources/local/models';
 
-const gitContentSource = new GitContentSource({
-    rootPath: __dirname,
-    contentDirs: ['content'],
-    models: Object.values(allModels),
-    assetsConfig: {
+export default defineStackbitConfig({
+  stackbitVersion: '~0.7.0',
+  ssgName: 'nextjs',
+  nodeVersion: '18',
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ['content'],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          labelField: "title",
+          fields: [{ name: "title", type: "string", required: true }]
+        },
+        {
+          name: "CarouselSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "items", type: "list" }]
+        },
+        {
+          name: "FeaturedItemsSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "items", type: "list" }]
+        },
+        {
+          name: "FeaturedPeopleSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "people", type: "list" }]
+        },
+        {
+          name: "FeaturedPostsSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "posts", type: "list" }]
+        },
+        {
+          name: "GenericSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "content", type: "string" }]
+        },
+        {
+          name: "ImageGallerySection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "images", type: "list" }]
+        },
+        {
+          name: "PagedPostsSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "posts", type: "list" }]
+        },
+        {
+          name: "PostFeedLayout",
+          type: "page",
+          filePath: "content/pages/post-feed.md",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }]
+        },
+        {
+          name: "PostFeedSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "posts", type: "list" }]
+        },
+        {
+          name: "PricingSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "plans", type: "list" }]
+        },
+        {
+          name: "RecentPostsSection",
+          type: "data",
+          labelField: "title",
+          fields: [{ name: "title", type: "string" }, { name: "posts", type: "list" }]
+        }
+      ],
+      assetsConfig: {
         referenceType: 'static',
         staticDir: 'public',
         uploadDir: 'images',
         publicPath: '/'
-    }
+      }
+    })
+  ],
+  siteMap: ({ documents, models }): SiteMapEntry[] => {
+    const pageModels = models.filter((model) => model.type === 'page').map((model) => model.name);
+    return documents
+      .filter((document) => pageModels.includes(document.modelName))
+      .map((document) => {
+        let slug = (document.fields.slug as DocumentStringLikeFieldNonLocalized)?.value;
+        if (!slug) return null;
+        slug = slug.replace(/^\/+/, '');
+        switch (document.modelName) {
+          case 'PostFeedLayout':
+            return {
+              urlPath: '/blog',
+              document: document
+            };
+          default:
+            return {
+              urlPath: `/${slug}`,
+              document: document
+            };
+        }
+      });
+  }
 });
-
-export const config = defineStackbitConfig({
-    stackbitVersion: '~0.7.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    styleObjectModelName: 'ThemeStyle',
-    contentSources: [gitContentSource],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['sources/local/presets']
-    },
-    siteMap: ({ documents, models }): SiteMapEntry[] => {
-        const pageModels = models.filter((model) => model.type === 'page').map((model) => model.name);
-        return documents
-            .filter((document) => pageModels.includes(document.modelName))
-            .map((document) => {
-                let slug = (document.fields.slug as DocumentStringLikeFieldNonLocalized)?.value;
-                if (!slug) return null;
-                /* Remove the leading slash in order to generate correct urlPath
-                regardless of whether the slug is '/', 'slug' or '/slug' */
-                slug = slug.replace(/^\/+/, '');
-                switch (document.modelName) {
-                    case 'PostFeedLayout':
-                        return {
-                            urlPath: '/blog',
-                            document: document
-                        };
-                    case 'PostLayout':
-                        return {
-                            urlPath: `/blog/${slug}`,
-                            document: document
-                        };
-                    default:
-                        return {
-                            urlPath: `/${slug}`,
-                            document: document
-                        };
-                }
-            });
-    }
-});
-
-export default config;
